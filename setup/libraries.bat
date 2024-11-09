177c7d26cc138e41c539c7801e6484818ea45f54
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-11-09 11:52:37 -0500

    Add lime
    duh

--- setup/libraries.bat
+++ setup/libraries.bat
@@ -1,10 +1,12 @@
 REM This contains all required libraries for this project
-haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
 haxelib set hxCodec 2.5.1
 haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-ui
+haxelib install lime
 haxelib install hscript
 haxelib install discord-rpc
 haxelib install linc_luajit
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
+haxelib run lime setup
 pause
