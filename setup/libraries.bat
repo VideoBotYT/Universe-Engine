527f7e3cdd29127f865c9d6f12465ea4c8cdb837
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-11-09 11:49:29 -0500

    Add a version to hxCodec
    Add an easy way to install required libraries

---
+++ setup/libraries.bat
@@ -0,0 +1,10 @@
+REM This contains all required libraries for this project
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
+haxelib set hxCodec 2.5.1
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install discord-rpc
+haxelib install linc_luajit
+pause
