1bb27bcf1f05437238fd57a98366207bef8d3009
Moxie <124418090+moxie-coder@users.noreply.github.com>
2024-11-14 03:09:08 -0500

    cleanup

--- setup/libraries.bat
+++ setup/libraries.bat
@@ -1,12 +1,14 @@
 REM This contains all required libraries for this project
-haxelib set hxCodec 2.5.1
+haxelib set hxCodec 2.5.1 --always
 haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-ui
-haxelib install lime
+haxelib install lime 8.1.3
+haxelib install openfl 9.2.2
 haxelib install hscript
-haxelib install discord-rpc
-haxelib install linc_luajit
+haxelib install hxcpp-debug-server
+haxelib git discord-rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
 haxelib run lime setup
 pause
