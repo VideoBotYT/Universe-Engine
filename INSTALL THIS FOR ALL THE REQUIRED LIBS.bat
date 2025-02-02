04d497394bce4f27d52ef9b04c480eeed367ef43
VideoBot <113830775+VideoBotYT@users.noreply.github.com>
2025-02-02 18:45:46 +0100

    Update INSTALL THIS FOR ALL THE REQUIRED LIBS.bat

--- INSTALL THIS FOR ALL THE REQUIRED LIBS.bat
+++ INSTALL THIS FOR ALL THE REQUIRED LIBS.bat
@@ -5,7 +5,7 @@ haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install lime
 haxelib install hscript
-haxelib install discord-rpc
+haxelib install discord_rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp.git
 haxelib run lime setup
